76d8fe14091136c900c45d21fd3ca0b8227673ed
hyes <hyeyeoun.joo@gmail.com>
2015-04-01 23:11:41 +0900

    Navigation Drawer 시작

--- app/src/main/java/hudi/android/hyes/ImFine_v03/MainActivity.java
+++ app/src/main/java/hudi/android/hyes/ImFine_v03/MainActivity.java
@@ -1,10 +1,17 @@
 package hudi.android.hyes.ImFine_v03;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -18,11 +25,17 @@ import it.gmariotti.cardslib.library.view.CardListView;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+    private ListView leftDrawerList;
+    private ArrayAdapter<String> navigationDrawerAdapter;
+    private String[] leftSliderData = {"현재 접속중인 아이 계정", "OOO(남 2개월 | 만0세)", "계정 변경하기", "신규가입하기", "계정추가하기"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_fragment_native_list_expand);
-
 
         ArrayList<Card> cards = new ArrayList<Card>();
         for (int i=0;i<200;i++){
@@ -36,9 +49,55 @@ public class MainActivity extends ActionBarActivity {
         if (listView!=null){
             listView.setAdapter(mCardArrayAdapter);
         }
+
+
+        nitView();
+        if (toolbar != null) {
+            toolbar.setTitle("I'm Fine");
+            setSupportActionBar(toolbar);
+        }
+        initDrawer();
+
+    }
+
+    private void nitView() {
+        leftDrawerList = (ListView) findViewById(R.id.left_drawer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationDrawerAdapter = new ArrayAdapter<String>( MainActivity.this,
+                android.R.layout.simple_list_item_1, leftSliderData);
+        leftDrawerList.setAdapter(navigationDrawerAdapter);
+    }
+
+    private void initDrawer() {
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
     }
 
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
     /**
      * This method builds a standard header with a custom expand/collpase
      */
@@ -101,6 +160,10 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
